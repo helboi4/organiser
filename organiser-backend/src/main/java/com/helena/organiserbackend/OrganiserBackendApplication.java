0f214a32ee62a1f0b56ba6a812922cdccf66791a
helboi4 <h.hopecarter@gmail.com>
2023-04-27 13:36:20 +0100

    first commit

---
+++ organiser-backend/src/main/java/com/helena/organiserbackend/OrganiserBackendApplication.java
@@ -0,0 +1,13 @@
+package com.helena.organiserbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganiserBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganiserBackendApplication.class, args);
+	}
+
+}
